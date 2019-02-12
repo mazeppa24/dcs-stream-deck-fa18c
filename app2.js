d0b8412c94f5578bed014368d9ed8c1e3fdfe088
mazeppa <mazeppa@nym.hush.com>
2019-02-12 21:11:20 +0100

    - ap page / ufc main page

--- app2.js
+++ app2.js
@@ -4,12 +4,15 @@ const StreamDeckApi = require('stream-deck-api-mazeppa');
 const DcsBiosApi = require('dcs-bios-api-mazeppa');
 const Path = require('path');
 const Logger = require('logplease');
+const Jimp = require('jimp');
 
 const logger = Logger.create('dcs-stream-deck-fa18');
 const api = new DcsBiosApi({logLevel: 'INFO'});
 const streamDeck = StreamDeckApi.getStreamDeck();
 
 const IMAGE_FOLDER = './resources/images/';
+const _textCache = {};
+const ICON_SIZE = 72;
 
 
 // connect to dcs-bios
@@ -28,6 +31,21 @@ if (streamDeck) {
 } else {
     logger.error('No Streamdeck connected! Please make sure Streamdeck is plugged in to you computer');
     process.exit(1);
+}
+
+String.prototype.centerJustify = function (length, char) {
+    var i = 0;
+    var str = this;
+    var toggle = true;
+    while (i + this.length < length) {
+        i++;
+        if (toggle)
+            str = str + char;
+        else
+            str = char + str;
+        toggle = !toggle;
+    }
+    return str;
 }
 
 // init
@@ -133,22 +151,6 @@ aircraftPages['FA-18C'] = {
         }
     },
     UFC:{
-
-        /*
-
-        1:  {type: 'page', page: 'MAIN', image: '/menus/button_back.png'},
-        2: {type: 'text', text: 'UFC', textColor: 'white', buttonColor: 'black'},
-        6:  {type: 'buttonGotoPage', button: 'UFC_AP', upImage: '/ufc/ufc_ap.png', downImage: '/ufc/ufc_ap_down.png', page: 'UFC_AP'},
-        7:  {type: 'button', button: 'UFC_IFF', upImage: '/ufc/ufc_iff.png', downImage: '/ufc/ufc_iff_down.png'},
-
-        11:  {type: 'buttonGotoPage', button: 'UFC_TCN', upImage: '/ufc/ufc_tcn.png', downImage: '/ufc/ufc_tcn_down.png', page: 'UFC_TCN'},
-        12: {type: 'button', button: 'UFC_ILS', upImage: '/ufc/ufc_ils.png', downImage: '/ufc/ufc_ils_down.png'},
-        13: {type: 'button', button: 'UFC_DL', upImage: '/ufc/ufc_dl.png', downImage: '/ufc/ufc_dl_down.png'},
-        14: {type: 'button', button: 'UFC_BCN', upImage: '/ufc/ufc_bcn.png', downImage: '/ufc/ufc_bcn_down.png'},
-        15: {type: 'button', button: 'UFC_ONOFF', upImage: '/ufc/ufc_on_off.png', downImage: '/ufc/ufc_on_off_down.png'},
-
-
-         */
         1:{
             //streamDeck.drawText(key.text, key.textColor, key.buttonColor, key.number);
             view: {type: 'page_image', page: 'MENU', image: '/menus/button_back.png', selImage: '/menus/button_back.png'},
@@ -162,16 +164,125 @@ aircraftPages['FA-18C'] = {
             view: {type: 'page_image', page: 'UFC_AP', image: '/ufc/ufc_ap.png', selImage: '/ufc/ufc_ap_down.png'},
             action: { type: 'page', page: 'UFC_AP' }
         },
-
         7: {
-            view: { type: 'state_image', input: 'UFC_AP',  states: { '0': '/ufc/ufc_ap.png', '1': '/ufc/ufc_ap_down.png' } },
-            action: { type: 'push_button', output: 'UFC_AP' }
+            view: { type: 'state_image', input: 'UFC_IFF',  states: { '0': '/ufc/ufc_iff.png', '1': '/ufc/ufc_iff_down.png' } },
+            action: { type: 'page', page: 'UFC_IFF' }
+        },
+        11: {
+            view: { type: 'state_image', input: 'UFC_TCN',  states: { '0': '/ufc/ufc_tcn.png', '1': '/ufc/ufc_iff_down.png' } },
+            action: { type: 'page', page: 'UFC_TCN' }
+        },
+        12: {
+            view: { type: 'state_image', input: 'UFC_ILS',  states: { '0': '/ufc/ufc_ils.png', '1': '/ufc/ufc_ils_down.png' } },
+            action: { type: 'page', page: 'UFC_ILS' }
+        },
+        13: {
+            view: { type: 'state_image', input: 'UFC_DL',  states: { '0': '/ufc/ufc_dl.png', '1': '/ufc/ufc_dl_down.png' } },
+            action: { type: 'page', page: 'UFC_DL' }
+        },
+        14: {
+            view: { type: 'state_image', input: 'UFC_BCN',  states: { '0': '/ufc/ufc_bcn.png', '1': '/ufc/ufc_bcn_down.png' } },
+            action: { type: 'page', page: 'UFC_BCN' }
+        },
+        15: {
+            view: { type: 'state_image', input: 'UFC_ONOFF',  states: { '0': '/ufc/ufc_on_off.png', '1': '/ufc/ufc_on_off_down.png' } },
+            action: { type: 'push_button', output: 'UFC_ONOFF' }
         },
     },
     UFC_AP:{
+
+        /*
+                1:  {type: 'page', page: 'UFC', image: '/menus/button_back.png'},
+        2: {type: 'text', text: 'UFC', textColor: 'white', buttonColor: 'black'},
+        3: {type: 'text', text: 'AP', textColor: 'white', buttonColor: 'black'},
+        11: {type: 'buttonDisplayUFC', button: 'UFC_OS1' ,upImage: '/ufc/ufc_ap/ufc_ap_atth_on.png', downImage: '/ufc/ufc_ap/ufc_ap_atth_off.png'},
+        12: {type: 'buttonGotoPageDisplayUFC',button: 'UFC_OS2', eventKey:'UFC_OPTION_CUEING_2', upImage: '/ufc/ufc_ap/ufc_ap_hsel_on.png', downImage: '/ufc/ufc_ap/ufc_ap_hsel_off.png', page: 'UFC_AP_HSEL'},
+        13: {type: 'buttonDisplayUFC', button: 'UFC_OS3', upImage: '/ufc/ufc_ap/ufc_ap_balt_on.png', downImage: '/ufc/ufc_ap/ufc_ap_balt_off.png'},
+        14: {type: 'buttonDisplayUFC', button: 'UFC_OS4', upImage: '/ufc/ufc_ap/ufc_ap_ralt_on.png', downImage: '/ufc/ufc_ap/ufc_ap_ralt_off.png'},
+         */
         1:{
             //streamDeck.drawText(key.text, key.textColor, key.buttonColor, key.number);
             view: {type: 'page_image', page: 'UFC_AP', image: '/menus/button_back.png', selImage: '/menus/button_back.png'},
+            action: { type: 'page', page: 'UFC' }
+        },
+        2:{
+            view: { type: 'page_text', text: 'UFC', textColor: 'white', buttonColor: 'black'},
+        },
+        3:{
+            view: { type: 'page_text', text: 'AP', textColor: 'white', buttonColor: 'black'},
+        },
+
+        // view: { type: 'state_label', text: 'LIGHTS', input: 'LANDING_LIGHTS', states: { '0': 'TAXI', '1': 'OFF', '2': 'LAND' }},
+        6: {
+            view: { type: 'state_label', text: 'ATTH', input: 'UFC_OPTION_CUEING_1',  states: { ':' : ':ATTH', ' ' : 'ATTH' } },
+        },
+        7: {
+            view: { type: 'state_label', text: 'HSEL', input: 'UFC_OPTION_CUEING_2',  states: { ':' : ':HSEL', ' ' : 'HSEL' } },
+        },
+        8: {
+            view: { type: 'state_label', text: 'BALT', input: 'UFC_OPTION_CUEING_3',  states: { ':' : ':BALT', ' ' : 'BALT' } },
+        },
+        9: {
+            view: { type: 'state_label', text: 'RALT', input: 'UFC_OPTION_CUEING_4',  states: { ':' : ':RALT', ' ' : 'RALT' } },
+        },
+        10: {
+            view: { type: 'state_label', text: 'HSEL', input: 'UFC_OPTION_CUEING_5',  states: { ':' :  'HSEL', ' ' : ' ' } },
+        },
+
+
+        11: {
+            view: { type: 'state_image', input: 'UFC_OS1',  states: { '0': '/ufc/ufc_on_off.png', '1': '/ufc/ufc_on_off_down.png' } },
+            action: { type: 'push_button', output: 'UFC_OS1' }
+        },
+        12: {
+            view: { type: 'state_image', input: 'UFC_OS2',  states: { '0': '/ufc/ufc_on_off.png', '1': '/ufc/ufc_on_off_down.png' } },
+            action: { type: 'push_button', output: 'UFC_OS2' }
+        },
+        13: {
+            view: { type: 'state_image', input: 'UFC_OS3',  states: { '0': '/ufc/ufc_on_off.png', '1': '/ufc/ufc_on_off_down.png' } },
+            action: { type: 'push_button', output: 'UFC_OS3' }
+        },
+        14: {
+            view: { type: 'state_image', input: 'UFC_OS4',  states: { '0': '/ufc/ufc_on_off.png', '1': '/ufc/ufc_on_off_down.png' } },
+            action: { type: 'push_button', output: 'UFC_OS4' }
+        },
+        15: {
+            view: { type: 'state_image', input: 'UFC_OS5',  states: { '0': '/ufc/ufc_on_off.png', '1': '/ufc/ufc_on_off_down.png' } },
+            action: { type: 'push_button', output: 'UFC_OS5' }
+        },
+    },
+    UFC_IFF:{
+        1:{
+            //streamDeck.drawText(key.text, key.textColor, key.buttonColor, key.number);
+            view: {type: 'page_image', page: 'UFC_IFF', image: '/menus/button_back.png', selImage: '/menus/button_back.png'},
+            action: { type: 'page', page: 'UFC' }
+        },
+    },
+    UFC_TCN:{
+        1:{
+            //streamDeck.drawText(key.text, key.textColor, key.buttonColor, key.number);
+            view: {type: 'page_image', page: 'UFC_TCN', image: '/menus/button_back.png', selImage: '/menus/button_back.png'},
+            action: { type: 'page', page: 'UFC' }
+        },
+    },
+    UFC_ILS:{
+        1:{
+            //streamDeck.drawText(key.text, key.textColor, key.buttonColor, key.number);
+            view: {type: 'page_image', page: 'UFC_ILS', image: '/menus/button_back.png', selImage: '/menus/button_back.png'},
+            action: { type: 'page', page: 'UFC' }
+        },
+    },
+    UFC_DL:{
+        1:{
+            //streamDeck.drawText(key.text, key.textColor, key.buttonColor, key.number);
+            view: {type: 'page_image', page: 'UFC_DL', image: '/menus/button_back.png', selImage: '/menus/button_back.png'},
+            action: { type: 'page', page: 'UFC' }
+        },
+    },
+    UFC_BCN:{
+        1:{
+            //streamDeck.drawText(key.text, key.textColor, key.buttonColor, key.number);
+            view: {type: 'page_image', page: 'UFC_BCN', image: '/menus/button_back.png', selImage: '/menus/button_back.png'},
             action: { type: 'page', page: 'UFC' }
         },
     }
@@ -241,7 +352,7 @@ initializeViewFn['state_label'] = function (view, key) {
                 view.currentImageBuffer = buffer;
                 draw(view)
             }).catch((buffer) => {
-            console.log(error)
+            //console.log(error)
         })
     }
     api.on(view.input, fn)
@@ -434,7 +545,6 @@ initializeActionFn['page'] = function (action, key) {
 function initializePages(pages) {
     Object.keys(pages).forEach((pageName) => {
         var page = pages[pageName];
-
         for (let i = 1; i <= 15; i++) {
             page[i] = page[i] || {};
 
@@ -500,6 +610,88 @@ function draw(view) {
         streamDeck.drawColor(0x000000, view.number);
     }
 }
+
+/**
+ * Return a font file with the specified type, size and color
+ * @param {String} name - only open-sans is supported
+ * @param {Integer} size - font size, 8 16 32 for color BLACK, 8 10 12 14 16 32 64 for color WHITE
+ * @param {String} color - BLACK or WHITE
+ */
+function getFontFile(name, size, color) {
+    return Path.resolve(__dirname, "./resources/fonts", name, name + "-" + size + "-" + color,  name + "-" + size + "-" + color + ".fnt" )
+}
+
+
+/**
+ * render a text string.
+ * @param {String} text - the string to draw
+ * @param {Object} options alignmentX, alignmentY, x, y, fontName, fontSize, fontColor, margin, backgroundColor
+ * @returns {Promise} promise with the image buffer or an error
+ */
+function renderText(text, options) {
+    // sensible defaults for options
+    options = Object.assign({}, {
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+        fontName: 'open-sans',
+        fontSize: 14,
+        fontColor: 'white',
+        margin: 0,
+        backgroundColor: 0x00000000,
+        bufferOnly: false
+    }, options || {})
+
+    // cache key
+    let cacheKey = {
+        alignmentX: options.alignmentX,
+        alignmentY: options.alignmentY,
+        fontFile: getFontFile(options.fontName, options.fontSize, options.fontColor),
+        x: options.x || options.margin,
+        y: options.y || options.margin,
+        text: text,
+        backgroundColor: options.backgroundColor
+    }
+
+    let hash = JSON.stringify(cacheKey)
+    let cachedImageBuffer = _textCache[hash]
+
+    return new Promise((resolve, reject) => {
+        if (cachedImageBuffer) {
+            resolve(cachedImageBuffer)
+        }
+        else {
+            Jimp.loadFont(cacheKey.fontFile).then(font => {
+                new Jimp(ICON_SIZE, ICON_SIZE, cacheKey.backgroundColor, (error, image) => {
+                    if (error) {
+                        console.log("Err: ", err, "Cache key: ", cacheKey)
+                        reject(error)
+                    } else {
+                        image.print(
+                            font,
+                            cacheKey.x,
+                            cacheKey.y,
+                            cacheKey,
+                            ICON_SIZE - cacheKey.x * 2,
+                            ICON_SIZE - cacheKey.y * 2,
+                            (err, image, { x, y }) => {
+                                if (err) {
+                                    console.log("Err: ", err, "Cache key: ", cacheKey)
+                                    reject(err)
+                                } else {
+                                    _textCache[hash] = image.bitmap.data
+                                    resolve(image.bitmap.data)
+                                }
+                            })
+                    }
+                })
+            }).catch(error => {
+                console.log("Err: ", error, "Cache key: ", cacheKey)
+                reject(error)
+            })
+        }
+    })
+}
+
 
 // START APP
 pages = aircraftPages['FA-18C'];
